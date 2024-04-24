339d28da4c55a0119bafa7c710aa5dbc81a11650
Jack Ooi <jack.ooi@bp.com>
2024-04-24 17:59:46 +0800

    g 	new file:   app/products/[productId]/reviews/[reviewsId]/not-found.tsx 	modified:   app/products/[productId]/reviews/[reviewsId]/page.tsx

--- app/products/[productId]/reviews/[reviewsId]/page.tsx
+++ app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewDetail({
   params,
 }: {
@@ -6,6 +8,9 @@ export default function ReviewDetail({
     reviewsId: string;
   };
 }) {
+  if (parseInt(params.reviewsId) > 1000) {
+    notFound();
+  }
   return (
     <h1>
       Review {params.reviewsId} for product {params.productId}
