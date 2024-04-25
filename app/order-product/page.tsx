d1e6ad7968655db400b283c1bcd28311aa595441
Jack Ooi <jack.ooi@bp.com>
2024-04-25 17:07:52 +0800

    Change logic to naviaget to home page

--- app/order-product/page.tsx
+++ app/order-product/page.tsx
@@ -1,18 +1,16 @@
 "use client";
-import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function OrderProdcut() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
   const handleClick = () => {
     console.log("Placing your order");
-    searchParams.get("/");
   };
   return (
     <>
       <h1>Order Prodcut</h1>
-      <button onClick={handleClick}>Place Order</button>
+      <button onClick={handleClick}>
+        <Link href={"/"}>Place Order</Link>
+      </button>
     </>
   );
 }
