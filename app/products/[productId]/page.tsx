83b0b7ad43fbbe930db8722400406cacabccaac8
Jack Ooi <jack.ooi@bp.com>
2024-04-25 14:29:10 +0800

    modified:   app/products/[productId]/page.tsx

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -8,13 +8,13 @@ export const generateMetadata = async ({
   params,
 }: Props): Promise<Metadata> => {
   const { productId } = params;
-  const titile = await new Promise((resolve) => {
+  const title = await new Promise((resolve) => {
     setTimeout(() => {
       resolve(`iPhone ${params.productId}`);
     }, 100);
   });
   return {
-    title: `Product ${titile}`,
+    title: `Product ${title}`,
   };
 };
 
