e7beff9c56b82ce9d87418b47239395f8bdc058d
Jack Ooi <jack.ooi@bp.com>
2024-04-25 14:14:16 +0800

    new file:   app/about/page.tsx 	modified:   app/page.tsx 	modified:   app/products/[productId]/page.tsx

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,7 +1,15 @@
-export default function ProductDetails({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: { productId: string };
-}) {
+};
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Product ${params.productId}`,
+  };
+};
+
+export default function ProductDetails({ params }: Props) {
   return <h1>Product Details {params.productId}</h1>;
 }
